fab706b8dc4222c43ab344b6a917b357f7c0a693
Joao Paulo Genari <122625272+jpgenari@users.noreply.github.com>
2023-06-18 18:53:36 +0100

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 /**
  * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed.
+ * and after the user's answer has been processed
  */
 function runGame(gameType) {
 
@@ -38,8 +38,8 @@ function runGame(gameType) {
 }
 
 /**
- * Checks the answer agains the first element in
- * the returned calculateCorrectAnswer array.
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
  */
 function checkAnswer() {
 
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -59,7 +61,7 @@ function checkAnswer() {
 
 /**
  * Get the operands (the numbers) and the operator (plus, minus, etc)
- * directly from the DOM, and returns the correct answer.
+ * directly from the DOM, and returns the correct answer
  */
 function calculateCorrectAnswer() {
 
@@ -75,11 +77,23 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Get the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
+    
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Get the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
